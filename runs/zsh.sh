0c8252926cde3a91f9b6edcd1cd339d21fb1f4e7
MaGnsi0 <magnsi0o0@gmail.com>
2025-08-10 01:14:30 +0300

    fix runs

--- runs/zsh.sh
+++ runs/zsh.sh
@@ -5,3 +5,5 @@ hash -r
 sudo chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "zsh & oh-my-zsh. Reboot in order to make zsh your default shell..."
