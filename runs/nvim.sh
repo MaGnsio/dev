00514bbf984f34e8195b2350708402e891b837b8
MaGnsi0 <magnsi0o0@gmail.com>
2025-08-10 00:21:37 +0300

    Hello from Arch

--- runs/nvim.sh
+++ runs/nvim.sh
@@ -2,5 +2,5 @@
 
 paru -S --noconfirm --needed neovim lua51 luarocks gcc cmake nodejs npm go
 paru -S --noconfirm --needed tree-sitter tree-sitter-cli fd fzf ripgrep
-git clone https://github.com/MaGnsio/nvim-config.git $HOME/.config/nvim
+git clone git@github.com:MaGnsio/nvim-config.git $HOME/.config/nvim
 sudo luarocks install luacheck
