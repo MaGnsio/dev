f5e9073d62738b82a69d7dd47bc1fef965eb269b
MaGnsi0 <magnsi0o0@gmail.com>
2025-10-12 04:30:52 +0300

    update

--- runs/update-configs.sh
+++ runs/update-configs.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-sudo cp ~/workspace/dev/configs/hypr/hyprland.conf ~/.config/hypr/ && hyprctl reload
+sudo cp ~/workspace/dev/configs/hypr/hyprland.conf ~/.config/hypr/
+sudo cp ~/workspace/dev/configs/hypr/hyprpaper.conf ~/.config/hypr/
+sudo cp ~/workspace/dev/configs/hypr/hyprlock.conf ~/.config/hypr/ && hyprctl reload
 
 sudo cp ~/workspace/dev/configs/ghostty/config ~/.config/ghostty/ && echo -e "\033[0;32m[action]\033[0m reload ghostty config"
 
+sudo cp ~/workspace/dev/configs/fontconfig/* ~/.config/fontconfig/ && fc-cache -f -v
+
+sudo cp ~/workspace/dev/configs/etc/ly/config.ini /etc/ly/
+
 sudo cp ~/workspace/dev/scripts/* /usr/local/bin/
+
