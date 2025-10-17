a84d7a835290e3354be2df9f537c88c70837bed6
MaGnsi0 <magnsi0o0@gmail.com>
2025-10-17 17:20:35 +0300

    update

--- runs/fonts.sh
+++ runs/fonts.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-# paru -S --noconfirm --needed ttf-jetbrains-mono-nerd ttf-ms-fonts ttf-segoe-ui-variable ttf-amiri
+# paru -S --noconfirm --needed ttf-ms-fonts ttf-segoe-ui-variable ttf-amiri
 paru -S --noconfirm --needed noto-fonts noto-fonts-emoji noto-fonts-cjk
 paru -S --noconfirm --needed otf-font-awesome otf-monaspace
+paru -S --noconfirm --needed ttf-jetbrains-mono-nerd
