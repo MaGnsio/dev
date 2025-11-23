c5931e50468e406b0257ae4fcbd63d107c207dd2
MaGnsi0 <magnsi0o0@gmail.com>
2025-11-23 04:59:12 +0200

    getting there 🔨

--- runs/rofi.sh
+++ runs/rofi.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
 
-paru -S --noconfirm --needed rofi papirus-icon-theme
-
-git clone https://github.com/Murzchnvok/rofi-collection
-
-cd rofi-collection
-
-cp -r tokyonight/tokyonight.rasi $HOME/.local/share/rofi/themes/
-
-cd .. && sudo rm -r rofi-collection
-
-CONFIG_FILE="$HOME/.local/share/rofi/themes/tokyonight.rasi"
-FONT_LINE='  font: "JetBrainsMono Nerd Font Bold 14";'
-sed -i "/font:/c\\$FONT_LINE" "$CONFIG_FILE"
-
+paru -S --noconfirm --needed rofi papirus-icon-theme rofimoji wtype
 echo "rofi setup done..."
--- runs/update-configs.sh
+++ runs/update-configs.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env zsh
 
-sudo cp ~/workspace/dev/configs/hypr/hyprland.conf ~/.config/hypr/
-sudo cp ~/workspace/dev/configs/hypr/hyprlock.conf ~/.config/hypr/ && hyprctl reload
+sudo cp ~/workspace/dev/configs/hypr/* ~/.config/hypr/ && hyprctl
 
 sudo cp ~/workspace/dev/configs/waybar/* ~/.config/waybar/
 
-sudo cp ~/workspace/dev/configs/ghostty/config ~/.config/ghostty/ && echo -e "\033[0;32m[action]\033[0m reload ghostty config"
+sudo cp ~/workspace/dev/configs/rofi/* ~/.config/rofi/
 
-sudo cp ~/workspace/dev/configs/fontconfig/* ~/.config/fontconfig/ && fc-cache -f -v
+sudo cp ~/workspace/dev/configs/rofimoji.rc ~/.config/rofimoji.rc
 
-sudo cp ~/workspace/dev/configs/etc/ly/config.ini /etc/ly/
+sudo cp ~/workspace/dev/configs/ghostty/config ~/.config/ghostty/ \
+  && echo -e "\033[0;32m[action]\033[0m reload ghostty config"
+
+sudo cp ~/workspace/dev/configs/fontconfig/* ~/.config/fontconfig/ \
+  && fc-cache -f -v
+
+sudo cp ~/workspace/dev/configs/etc/ly/config.ini /etc/ly/config.ini
 
 sudo cp ~/workspace/dev/scripts/* /usr/local/bin/
 
-sudo cp ~/workspace/dev/configs/.zshrc ~/.zshrc && source ~/.zshrc
+sudo cp ~/workspace/dev/configs/.zshrc ~/.zshrc \
+  && source ~/.zshrc
 
-sudo cp ~/workspace/dev/configs/.tmux.conf ~/.tmux.conf && tmux source -v ~/.tmux.conf
-
-sudo cp ~/workspace/dev/configs/rofi/config.rasi ~/.config/rofi/config.rasi
+sudo cp ~/workspace/dev/configs/.tmux.conf ~/.tmux.conf \
+  && tmux source -v ~/.tmux.conf
