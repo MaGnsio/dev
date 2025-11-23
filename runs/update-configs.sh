db909432727bd7355780475a50d0fbf90fca55ae
MaGnsi0 <magnsi0o0@gmail.com>
2025-11-23 05:14:30 +0200

    getting there 🔨

--- runs/update-configs.sh
+++ runs/update-configs.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env zsh
 
-sudo cp ~/workspace/dev/configs/hypr/* ~/.config/hypr/ && hyprctl
+sudo cp -r ~/workspace/dev/configs/hypr/* ~/.config/hypr/ && hyprctl
 
-sudo cp ~/workspace/dev/configs/waybar/* ~/.config/waybar/
+sudo cp -r ~/workspace/dev/configs/waybar/* ~/.config/waybar/
 
-sudo cp ~/workspace/dev/configs/rofi/* ~/.config/rofi/
+sudo cp -r ~/workspace/dev/configs/swaync/* ~/.config/swaync/
 
-sudo cp ~/workspace/dev/configs/rofimoji.rc ~/.config/rofimoji.rc
+sudo cp -r ~/workspace/dev/configs/rofi/* ~/.config/rofi/
 
-sudo cp ~/workspace/dev/configs/ghostty/config ~/.config/ghostty/ \
+sudo cp -r ~/workspace/dev/configs/rofimoji.rc ~/.config/rofimoji.rc
+
+sudo cp -r ~/workspace/dev/configs/ghostty/config ~/.config/ghostty/ \
   && echo -e "\033[0;32m[action]\033[0m reload ghostty config"
 
-sudo cp ~/workspace/dev/configs/fontconfig/* ~/.config/fontconfig/ \
+sudo cp -r ~/workspace/dev/configs/fontconfig/* ~/.config/fontconfig/ \
   && fc-cache -f -v
 
-sudo cp ~/workspace/dev/configs/etc/ly/config.ini /etc/ly/config.ini
+sudo cp -r ~/workspace/dev/configs/etc/ly/config.ini /etc/ly/config.ini
 
-sudo cp ~/workspace/dev/scripts/* /usr/local/bin/
+sudo cp -r ~/workspace/dev/scripts/* /usr/local/bin/
 
-sudo cp ~/workspace/dev/configs/.zshrc ~/.zshrc \
+sudo cp -r ~/workspace/dev/configs/.zshrc ~/.zshrc \
   && source ~/.zshrc
 
-sudo cp ~/workspace/dev/configs/.tmux.conf ~/.tmux.conf \
+sudo cp -r ~/workspace/dev/configs/.tmux.conf ~/.tmux.conf \
   && tmux source -v ~/.tmux.conf
